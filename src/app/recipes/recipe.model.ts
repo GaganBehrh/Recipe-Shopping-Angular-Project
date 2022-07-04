84dafe86c4bd75866370953a47258de932414412
Gagan Behrh <gaganpreet.behrh3@gmail.com>
2022-07-04 09:07:26 +0200

    recipe model added

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,12 @@
+export class Recipe { 
+    public name: string;
+    public description: string;
+    public imagePath: string;
+    constructor(name, desc, imagePath) { 
+        this.name = name;
+        this.description = desc;
+        this.imagePath = imagePath;
+    }
+
+
+}
